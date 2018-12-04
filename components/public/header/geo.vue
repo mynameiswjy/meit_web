9b352ad750305fa2bc529f3b0d47c525ba74ec0c
wangjiayun <wjy631438519@163.com>
2018-12-04 17:24:04 +0800

    first commit

---
+++ components/public/header/geo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="m-geo">
+    <i class="class"/>北京市
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity"
+    >
+      切换城市
+    </nuxt-link>
+    [大厂 香河 廊坊]
+  </div>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style scoped>
+
+</style>
