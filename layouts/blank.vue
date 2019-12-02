1f3285862204ca846bd1acc53fd8bf8a791c75ef
wangjiayun <wjy631438519@163.com>
2019-12-02 09:47:23 +0800

    commit

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>register</div>
+  <div class="layout-blank">
+    <nuxt/>
+  </div>
 </template>
 
 <script>
 	export default {
-	  layout: 'blank'
+		data() {
+		  return {
+      }
+    }
 	}
 </script>
 
